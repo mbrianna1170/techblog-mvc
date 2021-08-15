93c02013d3ae0c4ec3ae398ac35f1a67f9cca683
mbrianna1170 <brianna.miller09@yahoo.com>
2021-08-15 14:34:05 -0500

    comment model/associations set up

--- models/index.js
+++ models/index.js
@@ -1,13 +1,33 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require('./Comment');
 
-// create associations
+// create user associations
 User.hasMany(Post, {
   foreignKey: "user_id",
 });
 
+// create post associations
 Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+// create comment associations 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Comment };
